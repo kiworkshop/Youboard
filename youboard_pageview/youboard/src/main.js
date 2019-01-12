a6fd74eeb80a973680ceba73957f9b683ccab343
deocksoo <dusol92@snu.ac.kr>
2019-01-12 11:47:49 +0900

    [INITIAL] Fontawesome 라이브러리를 추가합니다
    소셜 아이콘을 사용하기 위해 fontawesome라이브러리를 채택하였습니다.

--- youboard_pageview/youboard/src/main.js
+++ youboard_pageview/youboard/src/main.js
@@ -1,8 +1,14 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import './plugins/vuetify'
+import '@fortawesome/fontawesome-free/css/all.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify, {
+  iconfont: 'fa5'
+})
 
 new Vue({
   render: h => h(App),
