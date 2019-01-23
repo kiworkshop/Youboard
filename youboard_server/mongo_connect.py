17ba280bab59c3dd8a3b6399d55de93aab3a3968
myang <kmh4503@gmail.com>
2019-01-23 23:20:16 +0900

    video scraper 플라스크 서버와 연동, result를 반환하도록 수정
    * 서버에서 해당 url을 치면 스크래핑 시작, string으로 result를 반환합니다.
    * TODO: result를 template으로 보여주기

--- youboard_server/mongo_connect.py
+++ youboard_server/mongo_connect.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 import channel_scraper
+import video_scraper
 
 app = Flask(__name__)
 
@@ -16,7 +17,22 @@ def scrap_channel():
     channels = channel_scraper.channel_scrap()
     for channel in channels:
         channel_collection.insert(channel)
-    return 'Completed to scrap channels!'
+    result = []
+    for channel in channel_collection.find():
+        result.append(channel)
+    return " ".join(str(x) for x in result)
+
+@app.route('/video-scraper')
+def scrap_video():
+    video_collection = mongo.db.video
+    video_collection.remove({ })
+    videos = video_scraper.video_scrap()
+    for video in videos:
+        video_collection.insert(video)
+    result = []
+    for video in video_collection.find():
+        result.append(video)
+    return " ".join(str(x) for x in result)
 
 if __name__ == '__main__':
     app.run(debug=True)
