1516e1569b5c9faa33019b273107e177f543d545
deocksoo <dusol92@snu.ac.kr>
2019-02-03 12:04:53 +0900

    [ADD] App.vue의 차트 분류 버튼에 기능을 추가합니다
    1. 버튼을 누르면 차트 분류가 변경되도록 하는 기능을 추가 하였습니다.
    2. 버튼을 눌러 차트 분류를 바꾸면 해당하는 버튼의 색이 바뀌도록 하였습니다.
    3. 윈도우 크기에서 버튼 사이즈를 화면 크기에 맞추어 조정하였습니다.
    4. 반복되는 코드를 v-for를 이용해 리펙토링 하였습니다.

--- youboard_pageview/youboard/src/App.vue
+++ youboard_pageview/youboard/src/App.vue
@@ -14,36 +14,29 @@
           </v-flex>
         </v-layout>
         <v-layout row justify-space-between class='pl-2 pr-2 mt-1 mb-1'>
-          <v-flex xs4 class="mr-1 ml-1">
-            <v-btn block>유튜버</v-btn>
-          </v-flex>
-          <v-flex xs4 class="mr-1 ml-1">
-            <v-btn block>채널</v-btn>
-          </v-flex>
-          <v-flex xs4 class="mr-1 ml-1">
-            <v-btn block>인기영상</v-btn>          
+          <v-flex xs4 v-for="(item, index) in classification" :key="index" class="mr-1 ml-1">
+            <v-btn block :color="item.criteria === activated ? '#F42072' : '#A9A9A9'" @click="view=item.chart; activated=item.criteria;">
+              <strong style="color: white; font-size: 4.3vw;">{{ item.criteria }}</strong>
+            </v-btn>
           </v-flex>
         </v-layout>
       </v-layout>
       <v-layout v-else row wrap align-center class="mt-3 pl-3 pr-3">
-        <v-flex sm2>
-          <v-img src="https://github.com/Deocksoo/my-first-blog/blob/master/blog/templates/blog/logo.png?raw=true" alt=""></v-img>
+        <v-flex shrink>
+          <v-img src="https://github.com/Deocksoo/my-first-blog/blob/master/blog/templates/blog/logo.png?raw=true" :width="window.width*0.2"></v-img>
         </v-flex>
-        <v-flex md2 lg1 class="mr-1 ml-1">
-          <v-btn block>유튜버</v-btn>
-        </v-flex>
-        <v-flex md2 lg1 class="mr-1 ml-1">
-          <v-btn block>채널</v-btn>
-        </v-flex>
-        <v-flex md2 lg1 class="mr-1 ml-1">
-          <v-btn block>인기영상</v-btn>          
+        <v-flex md1 lg1 v-for="(item, index) in classification" :key="index" class="mr-1 ml-1">
+          <v-btn block :color="item.criteria === activated ? '#F42072' : '#A9A9A9'" @click="view=item.chart; activated=item.criteria;">
+            <strong style="color: white; font-size: 1.5vw;">{{ item.criteria }}</strong>
+            </v-btn>
         </v-flex>
         <v-spacer></v-spacer>
         <v-flex md1>
           <v-icon large color="black">fas fa-search</v-icon>
         </v-flex>
       </v-layout>
-      <main-chart :window="window"></main-chart>
+      <component :is="view" :window="window"></component>
+      <!-- <main-chart :window="window"></main-chart> -->
     </v-layout>
   </v-app>
 </template>
@@ -63,7 +56,26 @@ export default {
       window: {
         width: 0,
         height: 0
-      }      
+      },
+      classification: [
+        {
+          criteria: "유투버",
+          chart: "MainChart",
+          dataKey: "Youtuber"
+        },
+        {
+          criteria: "채널",
+          chart: "MainChart",
+          dataKey: "Channel"
+        },
+        {
+          criteria: "인기영상",
+          chart: "PopularChart",
+          dataKey: "PopularVideo"
+        }
+      ],
+      activated: "유투버",
+      view: MainChart
     }
   },
   created() {
