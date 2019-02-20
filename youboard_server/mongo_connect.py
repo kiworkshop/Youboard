ca69dbee9cbc1e676b22bebd94f079667cd2d8e2
deocksoo <dusol92@snu.ac.kr>
2019-02-20 15:33:28 +0900

    [ADD] 애플리케이션 서버에 DB 조회 기능을 추가합니다
    route를 /channel로 접근하는 경우 channel 데이터를 json 객체로 리턴해주는
    함수를 구현하였습니다.

--- youboard_server/mongo_connect.py
+++ youboard_server/mongo_connect.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import json
 from flask_pymongo import PyMongo
 import channel_scraper
 import video_scraper
@@ -9,6 +10,22 @@ app.config['MONGO_DBNAME'] = 'youboard'
 app.config['MONGO_URI'] = 'mongodb://youboard:youboard2019@ds155614.mlab.com:55614/youboard'
 
 mongo = PyMongo(app)
+
+@app.route('/channel')
+def get_channel_data():
+    channel_collection = mongo.db.channel
+    rank_data = {}
+    for channel in channel_collection.find():
+        channel.pop("_id")
+        rank_data[channel["rank"]] = channel
+
+    response = app.response_class(
+        response=json.dumps(rank_data),
+        status=200,
+        mimetype='application/json'
+    )
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 @app.route('/channel-scraper')
 def scrap_channel():
