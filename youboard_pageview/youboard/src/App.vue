25509d24da1d8349c7d12930d273826750491719
Deocksoo <38533611+Deocksoo@users.noreply.github.com>
2019-03-03 23:18:13 +0900

    2.20 업데이트 사항입니다. (#17)
    * [INITIAL] Fontawesome 라이브러리를 추가합니다
    
    소셜 아이콘을 사용하기 위해 fontawesome라이브러리를 채택하였습니다.
    
    * [ADD] Youboard 페이지의 기본 구성을 추가합니다
    
    Youboard page는 '유투버, 채널, 인기영상'의 세 개 페이지로 구성되는데,
    유투버 페이지와 채널 페이지는 페이지 형식이 같습니다. 따라서, MainChart
    Component로 앞의 두 페이지를 구현하고, PopularChart component로
    인기영상 페이지를 구현합니다.
    
    App.vue에는 기본 구성인 공유버튼, 검색버튼, 페이지 로고, 페이지 이동
    버튼이 있으며, viewport 크기에 따라 반응형으로 구현하였습니다.
    
    * [EDIT] 유보드 메인 로고  이미지를 수정합니다
    
    1. 이미지 사이즈를 수정합니다.
    2. main-chart도 App.vue와 마찬가지로 반응형으로 만들기 위해 window size를
    props로 넘겨주도록 합니다.
    
    * [ADD] App.vue의 차트 분류 버튼에 기능을 추가합니다
    
    1. 버튼을 누르면 차트 분류가 변경되도록 하는 기능을 추가 하였습니다.
    2. 버튼을 눌러 차트 분류를 바꾸면 해당하는 버튼의 색이 바뀌도록 하였습니다.
    3. 윈도우 크기에서 버튼 사이즈를 화면 크기에 맞추어 조정하였습니다.
    4. 반복되는 코드를 v-for를 이용해 리펙토링 하였습니다.
    
    * [ADD] MongoDB접근을 위한 http 처리를 위해 axios를 사용합니다
    
    * [ADD] App.vue가 애플리케이션 서버에 데이터를 요청하도록 합니다
    
    1. 채널과 유투버, 인기영상 차트의 랭킹 데이터를 애플리케이션 서버에 요청하여
    받아오는 기능을 추가하였습니다.
    2. template grid를 일부 수정하였습니다.
    
    * [ADD] 애플리케이션 서버에 DB 조회 기능을 추가합니다
    
    route를 /channel로 접근하는 경우 channel 데이터를 json 객체로 리턴해주는
    함수를 구현하였습니다.
    
    * [EDIT] App.vue의 parser를 제거합니다
    
    서버의 리턴 타입을 json으로 바꾸게 되어 불필요한 parsing 기능을 제거하였습니다.
    
    * [ADD] main.js에서 axios를 import 하도록 합니다

--- youboard_pageview/youboard/src/App.vue
+++ youboard_pageview/youboard/src/App.vue
@@ -1,38 +1,124 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-layout column>
+      <v-flex>
+      <v-layout v-if="window.width < 960" column wrap>
+        <v-layout row wrap align-center justify-center class="mt-3 pl-3 pr-3">
+          <v-flex xs1 md1>
+            <v-icon large color="black">fas fa-share-alt</v-icon>
+          </v-flex>
+          <v-spacer></v-spacer>
+          <v-flex shrink>
+            <v-img src="https://github.com/Deocksoo/my-first-blog/blob/master/blog/templates/blog/logo.png?raw=true" :width="window.width*0.4"></v-img>
+          </v-flex>
+          <v-spacer></v-spacer>
+          <v-flex xs1>
+            <v-icon large color="black">fas fa-search</v-icon>
+          </v-flex>
+        </v-layout>
+        <v-layout row justify-space-between class='pl-2 pr-2 mt-1 mb-1'>
+          <v-flex xs4 v-for="(item, index) in classification" :key="index" class="mr-1 ml-1">
+            <v-btn block :color="item.criteria === activated ? '#F42072' : '#A9A9A9'" @click="view=item.chart; activated=item.criteria;">
+              <strong style="color: white; font-size: 4.3vw;">{{ item.criteria }}</strong>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-layout>
+      <v-layout v-else row wrap align-center class="mt-3 pl-3 pr-3">
+        <v-flex shrink>
+          <v-img src="https://github.com/Deocksoo/my-first-blog/blob/master/blog/templates/blog/logo.png?raw=true" :width="window.width*0.2"></v-img>
+        </v-flex>
+        <v-flex md1 lg1 v-for="(item, index) in classification" :key="index" class="mr-1 ml-1">
+          <v-btn block :color="item.criteria === activated ? '#F42072' : '#A9A9A9'" @click="view=item.chart; activated=item.criteria;">
+            <strong style="color: white; font-size: 1.5vw;">{{ item.criteria }}</strong>
+            </v-btn>
+        </v-flex>
+        <v-spacer></v-spacer>
+        <v-flex md1>
+          <v-icon large color="black">fas fa-search</v-icon>
+        </v-flex>
+      </v-layout>
+      </v-flex>
+      <v-flex>
+        <component :is="view" :window="window" :rankData="getRankData"></component>
+      </v-flex>
+    </v-layout>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MainChart from './components/MainChart'
+import PopularChart from './components/PopularChart'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainChart,
+    PopularChart
   },
   data () {
     return {
-      //
+      window: {
+        width: 0,
+        height: 0
+      },
+      classification: [
+        {
+          criteria: "유투버",
+          chart: "MainChart",
+          dataKey: "Youtuber"
+        },
+        {
+          criteria: "채널",
+          chart: "MainChart",
+          dataKey: "Channel"
+        },
+        {
+          criteria: "인기영상",
+          chart: "PopularChart",
+          dataKey: "PopularVideo"
+        }
+      ],
+      activated: "채널",
+      view: "",
+      rankData: {
+        channelData: {},
+        youtuberData: {},
+        popularData: {}
+      }
     }
-  }
+  },
+  created() {
+    window.addEventListener('resize', this.handleResize)
+    this.handleResize();
+  },
+  mounted() {
+    this.$http.get('http://127.0.0.1:5000/channel').then((result) => {
+      this.rankData.channelData = result.data
+      this.view = "MainChart"
+    })
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    handleResize() {
+      this.window.width = window.innerWidth;
+      this.window.height = window.innerHeight;
+    }
+  },
+  computed: {
+    getRankData() {
+      if (this.activated == '채널') {
+        return this.rankData.channelData;
+      }
+      else if (this.activated == '유투버') {
+        return this.rankData.youtuberData;
+      }
+      else if (this.activated == '인기영상') {
+        return this.rankData.popularData;
+      }
+    }
+  },
 }
 </script>
--- youboard_pageview/youboard/src/main.js
+++ youboard_pageview/youboard/src/main.js
@@ -1,8 +1,17 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import './plugins/vuetify'
+import '@fortawesome/fontawesome-free/css/all.css'
 import App from './App.vue'
+import axios from "axios";
+
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
+Vue.use(Vuetify, {
+  iconfont: 'fa5'
+})
 
 new Vue({
   render: h => h(App),
